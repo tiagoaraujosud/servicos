467ca199d5c3fecad14e68d125a860a15e357277
tiagoaraujosud <tiagoaraujosud@gmail.com>
2024-09-28 09:20:04 -0300

    Get Id

--- rest-api/model/portifolio/portifolioModel.js
+++ rest-api/model/portifolio/portifolioModel.js
@@ -4,4 +4,8 @@ module.exports = class PortifolioModel{
     static getTodos(callback){
         return db.query("SELECT * FROM servicos", callback);
     };  
+
+    static getId(id_servico, callback){
+        return db.query("SELECT * FROM servicos WHERE id_servico = ?", [id_servico], callback)
+    }
 };
--- rest-api/router/portifolioRouter.js
+++ rest-api/router/portifolioRouter.js
@@ -7,7 +7,6 @@ router.get("/", function(req, res, next){
 
     PortifolioModel.getTodos(function(erro, retorno){
         let resposta = new RespostaClass();
-
         if(erro){
             resposta.erro = true;
             resposta.msg = 'Ocorreu um erro';
@@ -15,7 +14,21 @@ router.get("/", function(req, res, next){
         }else{
             resposta.dados = retorno;
         }
+        res.json(resposta);
+    })
+})
 
+router.get("/:id_servico?", function(req, res, next){
+
+    PortifolioModel.getId(req.params.id_servico, function(erro, retorno){
+        let resposta = new RespostaClass();
+        if(erro){
+            resposta.erro = true;
+            resposta.msg = 'Ocorreu um erro';
+            console.log('erro', erro)
+        }else{
+            resposta.dados = retorno;
+        }
         res.json(resposta);
     })
 })
